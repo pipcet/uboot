355a7eac4bb9ec770919d2b62618c1dcf5b6026c
Mark Kettenis <kettenis@openbsd.org>
2021-05-23 19:55:07 +0200

    apple: Improve mailbox code

--- drivers/mailbox/apple-mbox.c
+++ drivers/mailbox/apple-mbox.c
@@ -4,12 +4,14 @@
  */
 
 #include <common.h>
+#include <clk.h>
 #include <dm.h>
 #include <mailbox-uclass.h>
 #include <asm/io.h>
 #include <linux/delay.h>
 
 #define REG_CPU_CTRL	0x0044
+#define  REG_CPU_CTRL_RUN	BIT(4)
 #define REG_A2I_STAT	0x8110
 #define  REG_A2I_STAT_EMPTY	BIT(17)
 #define  REG_A2I_STAT_FULL	BIT(16)
@@ -22,16 +24,29 @@
 #define REG_I2A_MSG1	0x8838
 
 struct apple_mbox_priv {
+	struct clk_bulk clks;
 	void *base;
 };
 
+phys_addr_t apple_mbox_phys_start;
+phys_addr_t apple_mbox_phys_addr;
+phys_size_t apple_mbox_size = SZ_64K;
+
 static void apple_mbox_wait(struct apple_mbox_priv *priv)
 {
-	while (readl(priv->base + REG_A2I_STAT) & REG_A2I_STAT_FULL)
-		udelay(1);
+	int retry;
 
-	while (readl(priv->base + REG_I2A_STAT) & REG_I2A_STAT_EMPTY)
+	retry = 500000;
+	while (--retry && readl(priv->base + REG_A2I_STAT) & REG_A2I_STAT_FULL)
 		udelay(1);
+	if (retry == 0)
+		printf("%s: A2I timeout\n", __func__);
+
+	retry = 500000;
+	while (--retry && readl(priv->base + REG_I2A_STAT) & REG_I2A_STAT_EMPTY)
+		udelay(1);
+	if (retry == 0)
+		printf("%s: I2A timeout\n", __func__);
 }
 
 static int apple_mbox_send(struct mbox_chan *chan, const void *data)
@@ -53,8 +68,8 @@ static int apple_mbox_recv(struct mbox_chan *chan, void *data)
 	u64 *msg = data;
 
 retry:
-	while (readl(priv->base + REG_I2A_STAT) & REG_I2A_STAT_EMPTY)
-		udelay(1);
+	if (readl(priv->base + REG_I2A_STAT) & REG_I2A_STAT_EMPTY)
+		return -ENODATA;
 
 	msg[0] = readq(priv->base + REG_I2A_MSG0);
 	msg[1] = readq(priv->base + REG_I2A_MSG1);
@@ -70,6 +85,26 @@ struct mbox_ops apple_mbox_ops = {
 	.recv = apple_mbox_recv,
 };
 
+static int apple_mbox_clk_init(struct udevice *dev,
+			       struct apple_mbox_priv *priv)
+{
+	int ret;
+
+	ret = clk_get_bulk(dev, &priv->clks);
+	if (ret == -ENOSYS || ret == -ENOENT)
+		return 0;
+	if (ret)
+		return ret;
+
+	ret = clk_enable_bulk(&priv->clks);
+	if (ret) {
+		clk_release_bulk(&priv->clks);
+		return ret;
+	}
+
+	return 0;
+}
+
 static int apple_mbox_probe(struct udevice *dev)
 {
 	struct apple_mbox_priv *priv = dev_get_priv(dev);
@@ -79,15 +114,31 @@ static int apple_mbox_probe(struct udevice *dev)
 	int nendpoints = 0;
 	int msgtype;
 	u64 subtype;
-	int i;
+	u32 cpu_ctrl;
+	int i, ret;
 
 	priv->base = dev_read_addr_ptr(dev);
 	if (!priv->base)
 		return -EINVAL;
 
+	ret = apple_mbox_clk_init(dev, priv);
+	if (ret)
+		return ret;
+
+	if (apple_mbox_phys_start == 0) {
+		apple_mbox_phys_start = (phys_addr_t)memalign(SZ_64K, SZ_64K);
+		apple_mbox_phys_addr = apple_mbox_phys_start;
+		apple_mbox_size = SZ_64K;
+	}
+
 	/* EP0_IDLE */
-	writeq(0x0060000000000220, priv->base + REG_A2I_MSG0);
-	writeq(0x0000000000000000, priv->base + REG_A2I_MSG1);
+	cpu_ctrl = readl(priv->base + REG_CPU_CTRL);
+	if (cpu_ctrl & REG_CPU_CTRL_RUN) {
+		writeq(0x0060000000000220, priv->base + REG_A2I_MSG0);
+		writeq(0x0000000000000000, priv->base + REG_A2I_MSG1);
+	} else {
+		writel(cpu_ctrl | REG_CPU_CTRL_RUN, priv->base + REG_CPU_CTRL);
+	}
 
 	/* EP0_WAIT_HELLO */
 	apple_mbox_wait(priv);
@@ -152,18 +203,30 @@ wait_epmap:
 		writeq(0x0000000000000000, priv->base + REG_A2I_MSG1);
 	}
 
+wait_pwrok:
 	/* EP0_WAIT_PWROK (discard) */
-	apple_mbox_wait(priv);
-	msg[0] = readq(priv->base + REG_I2A_MSG0);
-	msg[1] = readq(priv->base + REG_I2A_MSG1);
-
-	/* EP0_WAIT_PWROK */
 	apple_mbox_wait(priv);
 	msg[0] = readq(priv->base + REG_I2A_MSG0);
 	msg[1] = readq(priv->base + REG_I2A_MSG1);
 
 	endpoint = msg[1] & 0xff;
 	msgtype = (msg[0] >> 52) & 0xff;
+	if (endpoint == 1 || endpoint == 4) {
+		u64 size = (msg[0] >> 44) & 0xff;
+
+		if (apple_mbox_phys_addr + (size << 12) >
+		    apple_mbox_phys_start + apple_mbox_size) {
+			printf("%s: out of memory\n", __func__);
+			return -ENOMEM;
+		}
+
+		msg[0] &= ~0xfffffffffff;
+		msg[0] |= apple_mbox_phys_addr;
+		writeq(msg[0], priv->base + REG_A2I_MSG0);
+		writeq(msg[1], priv->base + REG_A2I_MSG1);
+		apple_mbox_phys_addr += (size << 12);
+		goto wait_pwrok;
+	}
 	if (endpoint != 0) {
 		printf("%s: unexpected endpoint %d\n", __func__, endpoint);
 		return -EINVAL;
